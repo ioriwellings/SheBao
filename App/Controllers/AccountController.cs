b5357299039bff95aeade27da9252f6f85a2cdf5
clark <ben@langben.com>
2017-05-15 16:37:58 +0800

    用户

--- App/Controllers/AccountController.cs
+++ App/Controllers/AccountController.cs
@@ -28,14 +28,15 @@ namespace Langben.App.Controllers
         {
             try
             {
-                 //dynamic login = this.LoginCheck(lname, pwd, "3");//用户登录（调用API接口）
-
-                if (lname != "admin" || pwd != "123456")
-                {
-                    return Json(new { Code = "0", Message = "用户名或密码错误！" });
-                }
-                dynamic login = new { Code = "ok" };
-                if (login.Code == "ok")//登录成功
+                //  dynamic login = this.LoginCheck(lname, pwd, "3");//用户登录（调用API接口）
+                BLL.AccountBLL account = new BLL.AccountBLL();
+                var data = account.ValidateUser(lname, pwd);
+                //if (lname != "admin" || pwd != "123456")
+                //{
+                //    return Json(new { Code = "0", Message = "用户名或密码错误！" });
+                //}
+                //dynamic login = new { Code = "ok" };
+                if (data != null)//登录成功
                 {
                     HttpCookie loginCookie = new HttpCookie("Login");
 
@@ -58,20 +59,20 @@ namespace Langben.App.Controllers
                     //loginCookie.Values["CompanyCityCode"] = login.Result.CompanyCityCode;
 
                     loginCookie.Values["CompanyID"] = "1";
-                    loginCookie.Values["CompanyName"] = "狼奔";
-                    loginCookie.Values["UserID"] = "1";//登录人ID
-                    loginCookie.Values["UserCode"] = "001";//人员编码（暂时无用）
-                    loginCookie.Values["LoginName"] = "admin";//登录名
-                    loginCookie.Values["RealName"] = "刘腾飞";//登录人真实姓名
-                    loginCookie.Values["Sex"] = "男";//性别
-                    loginCookie.Values["DepartmentID"] = "1";//所属部门ID
-                    loginCookie.Values["DepartmentName"] = "研发中心";//所属部门名称
+                    loginCookie.Values["CompanyName"] = "创英";
+                    loginCookie.Values["UserID"] = data.ID.ToString();//登录人ID
+                    loginCookie.Values["UserCode"] = data.ID.ToString();//人员编码（暂时无用）
+                    loginCookie.Values["LoginName"] = lname;//登录名
+                    loginCookie.Values["RealName"] = data.RName;//登录人真实姓名
+                    loginCookie.Values["Sex"] = data.Sex;//性别
+                    loginCookie.Values["DepartmentID"] = data.ORG_Department_ID.ToString();//所属部门ID
+                    loginCookie.Values["DepartmentName"] = "创英";//所属部门名称
                     loginCookie.Values["BranchID"] = "1";//直属机构ID
-                    loginCookie.Values["BranchName"] = "狼奔科技";//直属机构名称
+                    loginCookie.Values["BranchName"] = "创英";//直属机构名称
                     loginCookie.Values["PositionID"] = "2";//所属岗位ID
-                    loginCookie.Values["PositionName"] = "开发工程师";//所属岗位名称
-                    loginCookie.Values["UserName"] = "【研发中心】刘腾飞";
-                    loginCookie.Values["CompanyCityCode"] = "110100";
+                    loginCookie.Values["PositionName"] = "创英";//所属岗位名称
+                    loginCookie.Values["UserName"] = "创英";
+                    loginCookie.Values["CompanyCityCode"] = "110199";
 
                     #region 获取用户权限
                     Database dbEntity = EnterpriseLibraryContainer.Current.GetInstance<Database>("connStr");
@@ -100,8 +101,8 @@ namespace Langben.App.Controllers
                 }
                 else
                 {
-                    string code = login.Code;
-                    string message = login.Message;
+                    string code = "0";
+                    string message = "用户名或密码错误！";
                     return Json(new { Code = code, Message = message });
                 }
             }
